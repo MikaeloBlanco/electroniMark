3567ca25a925b8f57347261732f2b11bd007e1b7
AntoñitoElMetralleta <93702741+aloprio@users.noreply.github.com>
2024-02-20 01:08:19 +0100

    Subida del resto de endpoints (Antonio)
    Ya los tenia hecho, he añadido el README con un poco de info sobre ellos, no habia subido nada antes, el commit anterior es de lo que tenía en el PC de escritorio que fue cuando lo empecé seguí en el portatil y es el trabajo que tengo hecho hasta ahora.
    
    Adjunto mi base de datos por si acaso fallara cualquier cosa, pero vamos que el Flask debería de crearla en la carpeta raiz del proyecto

--- prueba.py
+++ prueba.py
@@ -20,14 +20,69 @@ def obtener_empleados():
     empleados_json = [{"id": emp.id, "nombre": emp.nombre, "rol": emp.rol, "sede" : emp.sede} for emp in empleados]
     return jsonify(empleados_json)
 
+@app.route("/agregarEmpleado", methods=["POST"])
+def agregar_empleado():
+    if not request.json or not 'nombre' in request.json or not 'rol' in request.json or not 'sede' in request.json:
+        abort(400)
+    empleado = Empleados(nombre=request.json['nombre'], rol=request.json['rol'], sede=request.json['sede'])
+    db.session.add(empleado)
+    db.session.commit()
+    return jsonify({'empleado': empleado.nombre}), 201
+
+@app.route("/editarEmpleado/<int:id>", methods=["PUT"])
+def editar_empleado(id):
+    empleado = Empleados.query.get(id)
+    if empleado is None:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'nombre' in request.json:
+        empleado.nombre = request.json['nombre']
+    if 'rol' in request.json:
+        empleado.rol = request.json['rol']
+    if 'sede' in request.json:
+        empleado.sede = request.json['sede']
+    db.session.commit()
+    return jsonify({'empleado': empleado.nombre}), 200
+
+@app.route("/borrarEmpleado/<int:id>", methods=["DELETE"])
+def borrar_empleado(id):
+    empleado = Empleados.query.get(id)
+    if empleado is None:
+        abort(404)
+    db.session.delete(empleado)
+    db.session.commit()
+    return jsonify({'se ha borrado': True})
+
+@app.route("/empleado/<int:id>", methods=["GET"])
+def obtener_empleado(id):
+    empleado = Empleados.query.get(id)
+    if empleado is None:
+        abort(404)
+    empleado_json = {"id": empleado.id, "nombre": empleado.nombre, "rol": empleado.rol, "sede" : empleado.sede}
+    return jsonify(empleado_json)
+
+@app.route("/empleadosPorRol/<string:rol>", methods=["GET"])
+def obtener_empleados_por_rol(rol):
+    empleados = Empleados.query.filter_by(rol=rol).all()
+    empleados_json = [{"id": emp.id, "nombre": emp.nombre, "rol": emp.rol, "sede" : emp.sede} for emp in empleados]
+    return jsonify(empleados_json)
+
+@app.route("/empleadosPorSede/<string:sede>", methods=["GET"])
+def obtener_empleados_por_sede(sede):
+    empleados = Empleados.query.filter_by(sede=sede).all()
+    empleados_json = [{"id": emp.id, "nombre": emp.nombre, "rol": emp.rol, "sede" : emp.sede} for emp in empleados]
+    return jsonify(empleados_json)
+
+
 if __name__ == "__main__":
-    print("Creando todas las tablas...")
+    print("Crear tablas")
     with app.app_context():
         db.create_all()
-    print("Tablas creadas.")
-    print("Verificando si la base de datos existe...")
+    print("Tablas creadas")
+    print("Comprobando que la base de datos existe")
     if os.path.exists(db_path):
-        print("La base de datos existe.")
+        print("La base de datos existe")
     else:
-        print("La base de datos no existe.")
+        print("La base de datos no existe")
     app.run(debug=True)
